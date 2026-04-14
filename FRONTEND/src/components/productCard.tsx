0d48b607587731a1f7b4921d7380ef921643e6a9
ayoub <benayoub0908@gmail.com>
2026-04-14 14:31:07 +0800

    fix productCard

--- FRONTEND/src/components/productCard.tsx
+++ FRONTEND/src/components/productCard.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { useCart } from "../context/cart/cartContext";
 import { useNavigate } from "react-router";
 import { useAuth } from "../context/auth/AuthContext";
 import { IconButton } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import { useCart } from "../context/cart/CartContext";
 interface props {
   image: string;
   title: string;
